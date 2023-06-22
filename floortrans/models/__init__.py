a31f548d9b7d8d6ee8fa5f275ee79738d9fe8608
BKousha <131717071+BKousha@users.noreply.github.com>
2023-06-22 16:21:35 +0000

    adding folder name for get_model

--- floortrans/models/__init__.py
+++ floortrans/models/__init__.py
@@ -1,6 +1,6 @@
 from floortrans.models.hg_furukawa_original import *
 
-def get_model(name, folder_path,n_classes=None, version=None):
+def get_model(folder_path,name,n_classes=None, version=None):
     if name == 'hg_furukawa_original':
         model = hg_furukawa_original(folder_path=folder_path,n_classes=n_classes)
         model.init_weights()
